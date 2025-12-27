55b00d7ca1a2d4f1350e5a157f56fd9e2b72aeef
atom <sacha310704@gmail.com>
2025-12-27 21:07:03 +0300

    A function for checking for scripts has been added to utils

--- dev/Rust/vsh/src/utils.rs
+++ dev/Rust/vsh/src/utils.rs
@@ -1,5 +1,5 @@
 
-use std::{fs::File, io::Read, path::Path};
+use std::{fs::File, io::{Read, BufReader, BufRead}, path::Path};
 use anyhow;
 
 const USERSPACE_PATH: &str = "/etc/userspace.toml";
@@ -47,4 +47,29 @@ pub fn get_current_path() -> String {
     }
 
     current_path
+}
+
+// Check if a file is a script
+pub fn is_script(path: &str) -> anyhow::Result<bool> {
+    if !Path::new(path).exists() {
+        return Ok(false);
+    }
+
+    let lines = read_first_lines(path, 5)?;
+
+    for line in lines {
+        if line.starts_with("#!") && line.contains("vsh") {
+            return Ok(true);
+        }
+    }
+
+    Ok(false)
+}
+
+// Reading a specified number of initial lines in a file
+fn read_first_lines(filename: &str, n: usize) -> std::io::Result<Vec<String>> {
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    reader.lines().take(n).collect::<Result<Vec<_>, _>>()
 }
