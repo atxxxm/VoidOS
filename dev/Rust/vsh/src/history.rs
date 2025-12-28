d01a77e33b9b4fabf1e22a6b4b4906ac2d74618a
atom <sacha310704@gmail.com>
2025-12-28 10:52:13 +0300

    History implemented

--- dev/Rust/vsh/src/history.rs
+++ dev/Rust/vsh/src/history.rs
@@ -10,23 +10,51 @@ impl History {
         Self { history_array: Vec::new(), history_index: None }
     }
 
+    // Previous command
     pub fn previous_command(&mut self) {
-        todo!()
+        if self.history_array.is_empty() {
+            return;
+        }
+
+        if let Some(idx) = self.history_index {
+            if idx > 0 {
+                self.history_index = Some(idx - 1);
+            }
+        } else {
+            self.history_index = Some(self.history_array.len() - 1);
+        }
     }
 
+    // Next command
     pub fn next_command(&mut self) {
-        todo!()
+        if self.history_array.is_empty() {
+            return;
+        }
+
+        if let Some(idx) = self.history_index {
+            if idx < self.history_array.len() - 1 {
+                self.history_index = Some(idx + 1);
+            } else {
+                self.history_index = None;
+            }
+        }
     }
 
+    // Add command to history
     pub fn add_command(&mut self, command: &str) {
-        todo!()
+        self.history_array.push(command.to_string());
     }
 
+    // Get command from history
     pub fn get_command(&self) -> String {
-        todo!()
+        if let Some(idx) = self.history_index {
+            return self.history_array[idx].clone();
+        }
+
+        return String::new();
     }
 
     pub fn zero_index(&mut self) {
-        todo!()
+        self.history_index = None;
     }
 }
