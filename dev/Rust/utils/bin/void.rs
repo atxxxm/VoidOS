6f93609305af2c42c295a5ce641b267898da32d2
atom <sacha310704@gmail.com>
2025-12-30 12:45:15 +0300

    Added a greeting utility

---
+++ dev/Rust/utils/bin/void.rs
@@ -0,0 +1,14 @@
+use colored::Colorize;
+
+fn main() {
+    println!("{}\t{}", "    @@@@@@@@@@@@@@@@@@@@@   ".cyan(), "VOID OS".yellow().bold());
+    println!("{}\t{}", "   @@@@@  @@@@@@@@  @@@@@@   ".cyan(), "Kernel: Linux".yellow().bold());
+    println!("{}\t{}", "  @@@@@@  @@@@@@@@  @@@@@@@  ".cyan(), "Without: GNU".yellow().bold());
+    println!("{}\t{}", " @@@@@@@@  @@@@@@  @@@@@@@@@ ".cyan(), "Created: atxxxm".yellow().bold());
+    println!("{}", " @@@@@@@@@  @@@@  @@@@@@@@@@ ".cyan());
+    println!("{}", " @@@@@@@@@  @@@@  @@@@@@@@@@ ".cyan());
+    println!("{}", " @@@@@@@@@@  @@  @@@@@@@@@@ ".cyan());
+    println!("{}", "  @@@@@@@@@@    @@@@@@@@@@ ".cyan());
+    println!("{}", "   @@@@@@@@@@   @@@@@@@@@ ".cyan());
+    println!("{}", "    @@@@@@@@@@@@@@@@@@@@ ".cyan());
+}
